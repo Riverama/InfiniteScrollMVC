e23e0a20ab5ed7f61cfe9f711ae3b80ab0eb34f7
Riverama <cfrc2@hotmail.com>
2014-01-25 12:42:34 -0600

    Knockout Model Added
    bootstrap layout completed
    knockout model applied

--- InfiniteScrollMVC/Controllers/HomeController.cs
+++ InfiniteScrollMVC/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,18 +14,27 @@ namespace InfiniteScrollMVC.Controllers
             return View();
         }
 
-        public ActionResult About()
+        /// <summary>
+        /// Collects images from gallery and creates an Json object from anonymous result set
+        /// </summary>
+        /// <param name="page">Page to get</param>
+        /// <param name="rows">Number of images to retrieve</param>
+        /// <returns>Json: Collection of image details</returns>
+        public JsonResult GetGallery(int page = 0, int rows = 16)
         {
-            ViewBag.Message = "Your application description page.";
+            var dir = new DirectoryInfo(Server.MapPath("~/Images/gallery"));
+            if (dir.Exists)
+            {
+                var images = (from i in dir.GetFiles()
+                              select new {
+                                  Name = i.Name,
+                                  Path = "../Images/gallery/" + i.Name
+                              }).Skip(page * rows).Take(rows);
 
-            return View();
-        }
+                return Json(images, JsonRequestBehavior.AllowGet);
+            }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return null;
         }
     }
 }
---
+++ InfiniteScrollMVC/Scripts/app.js
@@ -0,0 +1,38 @@
+﻿var gallery;
+
+function galleryViewModel(url) {
+    var self = this;
+    var galleryUrl = url;
+    self.Page = ko.observable(0); //staring page
+    self.Rows = ko.observable(40); //starting number of images
+    self.Images = ko.observableArray([]);
+
+    // refresh gallery
+    self.Refresh = function () {
+        $.getJSON(url, { 'page': self.Page(), 'rows': self.Rows() }, function (data) {
+            $.each(data, function (x, i) {
+                self.Images.push(i);
+            });
+        });
+    };
+    // advance to next page
+    self.GetNextPage = function() {
+        var nextPage = self.Page() + 1;
+        self.Page(nextPage);
+        self.Refresh();
+    };
+
+    //initial image load
+    self.Refresh();
+}
+
+// hooks windows on scroll event
+// detects when reaching bottom of page
+$(window).scroll(function () {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        if (gallery) {
+            // get next page set
+            gallery.GetNextPage();
+        };
+    }
+});
